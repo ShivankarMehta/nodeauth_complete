49ca3f68e1fd162cec2ea8ea3a87d60f243a63d7
unknown <shivankarmehta60@gmail.com>
2024-11-28 17:10:12 +0900

    authentication done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const mongoose= require('mongoose');
 const authRoutes=require('./routes/authRoutes');
 const cookieParser=require('cookie-parser');
+const {requireAuth}= require('./middleware/authmiddleware');
 const app= express();
 
 
@@ -18,21 +19,10 @@ mongoose.connect(dbURI)
 .catch((err)=> console.log(err));
 
 app.get('/',(req,res)=> res.render('home'));
-app.get('/smoothies', (req,res)=> res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req,res)=> res.render('smoothies'));
 // app.get('/signup', (req,res)=>res.render('signup'));
 // app.get('/login', (req,res)=>res.render('login'));
 app.use(authRoutes);
 
-app.get('/set-cookies', (req,res)=>{
-// res.setHeader('Set-Cookie','newUser=true');
-res.cookie('newuser', false);
-res.cookie('isEmployee',true,{maxAge: 1000*60*60*24,httpOnly:true});
-res.send('you got the cookies');
-})
 
-app.get('/read-cookies', (req,res)=>{
-const cookies=req.cookies;
-console.log(cookies);
-res.json(cookies);
-})
 
